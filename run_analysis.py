868e37a832cf28817992b0e5ea41a0b3618f9961
vierth <p.a.vierthaler@hum.leidenuniv.nl>
2018-12-09 16:07:38 +0100

    add detect to mod

--- run_analysis.py
+++ run_analysis.py
@@ -15,7 +15,16 @@ from detect_intertexuality import detectIntertextuality
 # set the seed length
 seedLength = 4
 
+# How long is the minimal match?
+matchLength = 10
 
+# How similar is the minimum similarity?
+threshhold = .8
+
+# Set this to limit the similarity comparison to last n characters
+# Set to None for no limit. Setting a limit significantly
+# speeds the calculations up.
+maxComp = 100
 
 ###################
 # CORPUS CREATION #
@@ -27,6 +36,7 @@ corpusFolder = "corpus"
 # Name of corpus save file.
 pickleFile = "corpus.pickle"
 
+
 #################
 # INDEX OPTIONS #
 #################
@@ -34,9 +44,12 @@ pickleFile = "corpus.pickle"
 # Would you like to build an index? If so, set buildIndex to True. 
 buildIndex = True
 
-# Output corpus file
+# Output corpus file (should end with .db). Can be None if buildIndex is false
 indexFile = 'index.db'
 
+################
+# RUN FUNCTION #
+################
 
 def run():
     # Create the corpus file. You can optionally specify if you want to remove
@@ -45,12 +58,15 @@ def run():
     # createCorpus(corpusFolder, pickleFile)
 
     # Create an index if desired:
-    if buildIndex:
-        print("Creating Index")
-        formIndex(seedLength, pickleFile, indexFile)
+    #if buildIndex:
+    #    print("Creating Index")
+    #    formIndex(seedLength, pickleFile, indexFile)
 
     # Run the main intertextuality algorithm
-    detectIntertextuality(seedLength, pickleFile)
+    
+    detectIntertextuality(seedLength, matchLength, threshhold, maxComp, 
+                          pickleFile, hasPreppedIndex=buildIndex, 
+                          indexFile=indexFile)
 
 if __name__ == "__main__":
     run()
